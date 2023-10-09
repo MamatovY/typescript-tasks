cdc1d296212913007034943efd61353690e01234
MamatovY <mamatovyiman28@gmail.com>
2023-10-09 12:47:29 +0600

    init 08-overloads asyncSum()

--- src/3/08-overloads.ts
+++ src/3/08-overloads.ts
@@ -4,3 +4,16 @@ function add(a: string, b: string): string
 function add(a: any, b: any): any {
     return a + b
 }
+
+type asyncCb = (res: number) => number
+
+function asyncSum(a: number, b: number): Promise<number>
+function asyncSum(a: number, b: number, cb: asyncCb): number
+function asyncSum(a: number, b: number, cb?: asyncCb): any {
+    const result = a + b
+    if (cb) {
+        return cb(result)
+    }
+
+    return Promise.resolve(result)
+}
